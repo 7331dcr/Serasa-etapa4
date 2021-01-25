202e7c184df59fa44e4f82aad5d974ee92fc3f87
rcd1337 <cs50hypers@gmail.com>
2021-01-25 04:42:43 -0300

    added two API Routes

--- alf/gradesmanager/urls.py
+++ alf/gradesmanager/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path ("", views.index, name="index")
+
+    # API Routes
+    path("gabarito", views.gabarito, name="gabarito"),
+    path("resposta", views.resposta, name="resposta")
 ]
