6bf4b5dbf3bc8a9b5128e48d0e27660ff92b74e6
rcd1337 <cs50hypers@gmail.com>
2021-01-27 06:57:10 -0300

    started cadastrar_gabarito view

--- alf/gradesmanager/views.py
+++ alf/gradesmanager/views.py
@@ -1,12 +1,34 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Aluno, Gabarito, Prova
 
 # Create your views here.
 
 def index(request):
     return render( request, "gradesmanager/index.html")
 
+@csrf_exempt
 def cadastrar_gabarito(request):
-    return False
+    
+    if request.method != "POST":
+        return JsonResponse({"error":"POST request required."}, status=400)
+
+    
+     data = json.loads(request.body)
+
+    # if not data:
+    #     return JsonResponse({"error":"No data was sent"}, status=400)
+    
+    # gabarito = Gabarito(gabarito=data)
+    # try:
+    #     gabarito.save()
+    # except:
+    #     return JsonResponse({"error": "Data not saved"}, status=400)
+    
+    return JsonResponse({"message": "Data successfully saved"}, status=201)
 
 def cadastrar_resposta(request):
     return False
