c12fcd5730aa695a831d8ce4322223f868b552f8
rcd1337 <cs50hypers@gmail.com>
2021-01-28 11:44:52 -0300

    reformated models.py

--- alf/gradesmanager/models.py
+++ alf/gradesmanager/models.py
@@ -6,10 +6,18 @@ class Aluno(models.Model):
     aprovado = models.BooleanField()
 
 class Gabarito(models.Model):
-    gabarito = models.TextField()
+    pergunta = models.TextField()
+    resposta = models.TextField()
 
 class Prova(models.Model):
-    aluno_id = models.ForeignKey(Aluno, on_delete=models.PROTECT)
-    gabarito_id = models.ForeignKey(Gabarito, on_delete=models.PROTECT)
-    respostas = models.TextField()
-    nota = models.DecimalField(max_digits=3, decimal_places=1)
+    aluno_id = models.ForeignKey(Aluno, on_delete=models.PROTECT, blank=True)
+    gabarito_id = models.ForeignKey(Gabarito, on_delete=models.PROTECT, blank=True)
+    respostas_id = models.ForeignKey('Respostas', on_delete=models.PROTECT, blank=True)
+    nota = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+
+class Respostas(models.Model):
+    prova_id = models.ForeignKey(Prova, on_delete=models.CASCADE)
+    pergunta = models.TextField()
+    resposta = models.TextField()
+
+
