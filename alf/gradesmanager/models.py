20cc726176959e218cf834f43a01e6455ed311f2
rcd1337 <cs50hypers@gmail.com>
2021-01-28 22:04:10 -0300

    rearranged database

--- alf/gradesmanager/models.py
+++ alf/gradesmanager/models.py
@@ -3,20 +3,22 @@ from django.db import models
 # Create your models here.
 class Aluno(models.Model):
     nome = models.CharField(max_length=255)
-    aprovado = models.BooleanField()
+    aprovado = models.BooleanField(null=True)
+
+class Prova(models.Model):
+    aluno_fk = models.ForeignKey(Aluno, on_delete=models.PROTECT, blank=True, null=True)
+    gabarito_fk = models.ForeignKey('Gabarito', on_delete=models.PROTECT, blank=True, null=True)
+    respostas_fk = models.ForeignKey('Respostas', on_delete=models.PROTECT, blank=True, null=True)
+    nota = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
+    nome_prova = models.CharField(max_length=255, blank=True)
 
 class Gabarito(models.Model):
+    prova_fk = models.ForeignKey(Prova, on_delete=models.CASCADE)
     pergunta = models.TextField()
     resposta = models.TextField()
 
-class Prova(models.Model):
-    aluno_id = models.ForeignKey(Aluno, on_delete=models.PROTECT, blank=True)
-    gabarito_id = models.ForeignKey(Gabarito, on_delete=models.PROTECT, blank=True)
-    respostas_id = models.ForeignKey('Respostas', on_delete=models.PROTECT, blank=True)
-    nota = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
-
 class Respostas(models.Model):
-    prova_id = models.ForeignKey(Prova, on_delete=models.CASCADE)
+    prova_fk = models.ForeignKey(Prova, on_delete=models.CASCADE)
     pergunta = models.TextField()
     resposta = models.TextField()
 
