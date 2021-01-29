2c94d3cde09a6ce9993ed53efb2c811c755d3ec3
rcd1337 <cs50hypers@gmail.com>
2021-01-29 03:20:11 -0300

    udpated models.py

--- alf/gradesmanager/models.py
+++ alf/gradesmanager/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Aluno(models.Model):
     nome = models.CharField(max_length=255)
-    aprovado = models.BooleanField(null=True)
+    aprovado = models.BooleanField(default=False)
 
 class Prova(models.Model):
     aluno_fk = models.ForeignKey(Aluno, on_delete=models.PROTECT, blank=True, null=True)
-    nota = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
+    nota = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
     nome_prova = models.CharField(max_length=255, blank=True)
 
 class Gabarito(models.Model):
